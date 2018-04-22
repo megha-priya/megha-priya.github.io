4930342e1e44cd903948a58d131425460e5121b4
Piyush Singh <piyush.0966@gmail.com>
2018-04-22 06:11:27 +0530

    first draft

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 window.addEventListener("load",function(){
-    setTimeout(function(){
-      var load_screen = document.getElementById("loading");
-      document.body.removeChild(load_screen);
-    },3000);
-    // var load_screen = document.getElementById("loading");
-    // document.body.removeChild(load_screen);
+    // setTimeout(function(){
+    //   var load_screen = document.getElementById("loading");
+    //   document.body.removeChild(load_screen);
+    // },3000);
+    var load_screen = document.getElementById("loading");
+    document.body.removeChild(load_screen);
   });
 
   jQuery(document).ready(function($) {
